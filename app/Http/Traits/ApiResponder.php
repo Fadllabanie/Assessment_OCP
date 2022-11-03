5bacc51daf32e0524c942f68d7308bb44f37a359
fadellabanie <fadel.labanie@gmail.com>
2022-11-03 13:49:41 +0200

    fix

--- app/Http/Traits/ApiResponder.php
+++ app/Http/Traits/ApiResponder.php
@@ -27,15 +27,7 @@ trait ApiResponder
     {
         return $this->success;
     }
-   /**
-     * Getter for statusCode
-     *
-     * @return int
-     */
-    public function getFailure()
-    {
-        return $this->failure;
-    }
+  
 
     /**
      * Setter for statusCode
@@ -55,71 +47,6 @@ trait ApiResponder
         return response()->json($data, $this->statusCode, $headers);
     }
 
-    public function successStatus($message = 'Success Request')
-    {
-        return $this->respond([
-            'status' => $this->getSuccess(),
-            'message' => $message,
-        ]);
-    }
-    public function errorStatus($message = 'Failure Request')
-    {
-        return $this->respond([
-            'status' => 0,
-            'message' => $message,
-        ]);
-    }
-
-    public function respondNoContent($message = 'No content')
-    {
-        return $this->setStatusCode(204)
-       ->respond([
-            'status' => $this->getFailure(),
-            'message' => $message,
-        ]);
-
-           
-    }
-
-    public function respondWithItem($item, $message = 'Success Request')
-    {
-        return $this->respond([
-            'status' => $this->getSuccess(),
-            'message' => $message,
-            'data' => $item,
-          
-        ]);
-    } 
-    public function respondWithItemName($item_name,$item, $message = 'Success Request')
-    {
-        return $this->respond([
-            'status' => $this->getSuccess(),
-            'message' => $message,
-            $item_name => $item,
-        ]);
-    }
-   
-
-    public function respondWithCollection($collection,$message='Success Request')
-    {
-        return $this->respond([
-            'status' => $this->getSuccess(),
-            'message' => $message,
-            'data' => $collection,
-        ]);
-    }
-
-
-    public function respondWithMessage($message)
-    {
-        return $this->setStatusCode(200)
-            ->respond([
-                'status' => $this->getSuccess(),
-                'message' => $message
-            ]);
-    }
-
-  
 
     public function respondCreated($message = 'Resource created successfully')
     {
@@ -132,78 +59,5 @@ trait ApiResponder
             ]);
     }
 
-    protected function respondWithError($message)
-    {
-        if (! is_array($message)) {
-            $message = [
-                'body' => [$message]
-            ];
-        }
-
-        return $this->respond([
-            'http_code' => $this->getStatusCode(),
-            'error' => [
-                'message' => $message,
-            ]
-        ]);
-    }
-
-    /**
-     * Generates a Response with a 403 HTTP header and a given message.
-     *
-     * @return Response
-     */
-    public function errorForbidden($message = 'Forbidden')
-    {
-        return $this->setStatusCode(403)
-          ->respondWithError($message);
-    }
-
-    /**
-     * Generates a Response with a 500 HTTP header and a given message.
-     *
-     * @return Response
-     */
-    public function errorInternalError($message = 'Internal Error')
-    {
-        return $this->setStatusCode(500)
-          ->respondWithError($message);
-    }
-  
-    /**
-     * Generates a Response with a 404 HTTP header and a given message.
-     *
-     * @return Response
-     */
-    public function errorNotFound($message = 'Resource Not Found')
-    {
-        return $this->setStatusCode(404)
-       ->respond([
-            'status' => $this->getFailure(),
-            'message' => $message,
-        ]);
-    }
-
-    /**
-     * Generates a Response with a 401 HTTP header and a given message.
-     *
-     * @return Response
-     */
-    public function errorUnauthorized($message = 'Unauthorized')
-    {
-        return $this->setStatusCode(401)
-          ->respondWithError($message);
-    }
-
-    /**
-     * Generates a Response with a 400 HTTP header and a given message.
-     *
-     * @return Response
-     */
-    public function errorWrongArgs($message)
-    {
-        return $this->setStatusCode(400)
-          ->respondWithError($message);
-    }
 
 }
